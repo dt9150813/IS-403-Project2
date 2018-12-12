d8e38cb43f8d7b9dbc03294551065cd6a0940a58
dt9150813 <eddy0712@gmail.com>
2018-12-11 18:05:03 -0700

    saqd

--- IS 403 Project2/Models/MissionInformation.cs
+++ IS 403 Project2/Models/MissionInformation.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace IS_403_Project2.Models
 {
+    [Table("Mission")]
     public class MissionInformation
     {
         public int MissionID { get; set; }
